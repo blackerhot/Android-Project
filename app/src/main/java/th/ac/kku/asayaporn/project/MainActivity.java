534668ab340f20816dd3b319c194ded2202a7906
blackerhot <knwknw1@gmail.com>
2019-02-25 01:00:33 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/kku/asayaporn/project/MainActivity.java
@@ -0,0 +1,13 @@
+package th.ac.kku.asayaporn.project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inside_main);
+    }
+}
