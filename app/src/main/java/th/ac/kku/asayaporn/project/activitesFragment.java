425d41891c4129acbdf25733ac4c83babbfac843
Chayanon <chayanon_b@kkumail.com>
2019-04-13 13:25:57 +0700

    chayanon

--- app/src/main/java/th/ac/kku/asayaporn/project/activitesFragment.java
+++ app/src/main/java/th/ac/kku/asayaporn/project/activitesFragment.java
@@ -12,37 +12,56 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class activitesFragment extends  Fragment {
     public TextView testtv;
     public Button btn_go_calen;
     Intent intentother = null;
 
+    int[] listviewImage = new int[]{
+            R.drawable.ic_menu_camera1, R.drawable.ic_menu_camera2, R.drawable.ic_menu_camera3, R.drawable.ic_menu_camera4,
+            R.drawable.ic_menu_camera5, R.drawable.ic_menu_camera6, R.drawable.ic_menu_camera7, R.drawable.ic_menu_camera8,R.drawable.ic_menu_camera9, R.drawable.ic_menu_camera10,
+    };
+
+    String[] listviewShortDescription = new String[]{
+            "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+            "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+    };
+
+    String[] listviewTitle = new String[]{
+            "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4",
+            "ListView Title 5", "ListView Title 6", "ListView Title 7", "ListView Title 8",
+    };
+
     @Nullable
     @Override
     public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
         View view = inflater.inflate(R.layout.fragment_activites,container,false);
-        /*
-        btn_go_calen.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                intentother = new Intent(Intent.ACTION_VIEW,
-                        Uri.parse("https://gsuite.google.com/intl/en_id/products/calendar/?utm_source=google&utm_medium=cpc&utm_campaign=japac-TH-all-en-dr-bkws-all-golden-trial-e-dr-1003989&utm_content=text-ad-none-none-DEV_c-CRE_336301664879-ADGP_Hybrid%20%7C%20AW%20SEM%20%7C%20BKWS%20~%20EXA%20%7C%20Calendar%20%7C%20%5B1:1%5D%20%7C%20TH%20%7C%20EN%20%7C%20google.calendar-KWID_43700041781917229-kwd-380869074165-userloc_9073383&utm_term=KW_google.calendar&gclid=Cj0KCQjwpsLkBRDpARIsAKoYI8yZQggj7xEHcQhzdQUTlFKgHpciR4-JktbRa7YEEFwfxs7BK5-bPygaAk-SEALw_wcB&gclsrc=aw.ds"));
-                startActivity(intentother);
-                testtv.setText("5555");
+
+
+            List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+
+            for (int i = 0; i < 8; i++) {
+                HashMap<String, String> hm = new HashMap<String, String>();
+                hm.put("listview_title", listviewTitle[i]);
+                hm.put("listview_discription", listviewShortDescription[i]);
+                hm.put("listview_image", Integer.toString(listviewImage[i]));
+                aList.add(hm);
             }
-        }); */
-        String[] lst = {"1","2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
-        ListView listView = (ListView) view.findViewById(R.id.lst);
-        listView.requestFocus();
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,lst
-        );
-        listView.setAdapter(listViewAdapter);
+
+            String[] from = {"listview_image", "listview_title", "listview_discription"};
+            int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(getContext(), aList, R.layout.listview_activity, from, to);
+            ListView androidListView = (ListView) view.findViewById(R.id.lst);
+            androidListView.setAdapter(simpleAdapter);
+
         return view;
     }
 }
