5464b7d7c81085c1992b1a1a735540d2adb8f7e9
Chayanon <chayanon_b@kkumail.com>
2019-04-17 22:14:13 +0700

    chayanon

--- app/src/main/java/th/ac/kku/asayaporn/project/activitesFragment.java
+++ app/src/main/java/th/ac/kku/asayaporn/project/activitesFragment.java
@@ -1,11 +1,15 @@
 package th.ac.kku.asayaporn.project;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.support.design.widget.SwipeDismissBehavior;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,9 +21,16 @@ import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import static com.facebook.FacebookSdk.getApplicationContext;
 
 public class activitesFragment extends  Fragment {
     public TextView testtv;
@@ -40,36 +51,72 @@ public class activitesFragment extends  Fragment {
             "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
             "Android ListView Short Description", "Android ListView Short Description"
     };
+    private static final String TAG = "MainActivity";
+
 
 
     @Nullable
     @Override
     public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle saveInstanceState) {
         View view = inflater.inflate(R.layout.fragment_activites,container,false);
+        SwipeMenuListView listView;
+        listView = (SwipeMenuListView) view.findViewById(R.id.lst);
 
 
-            List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
-        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.bgActivity);
+        List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+        // LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.bgActivity);
 
-            for (int i = 0; i < 10; i++) {
-                HashMap<String, String> hm = new HashMap<String, String>();
-                hm.put("listview_title", lstTime[i]);
-                hm.put("listview_discription", lstTitle[i]);
-                hm.put("listview_item", lstItems[i]);
+        for (int i = 0; i < 10; i++) {
+            HashMap<String, String> hm = new HashMap<String, String>();
+            hm.put("listview_title", lstTime[i]);
+            hm.put("listview_discription", lstTitle[i]);
+            hm.put("listview_item", lstItems[i]);
               /*  if( i % 2 ==0) {
                     linearLayout.setBackgroundResource(R.drawable.bgnotify2);
                 } else{
                     linearLayout.setBackgroundResource(R.drawable.bgnotify);
                 } */
-                aList.add(hm);
+            aList.add(hm);
+        }
+
+        String[] from = { "listview_title", "listview_discription", "listview_item"};
+        int[] to = {R.id.lstTime, R.id.lstTitle, R.id.lstItems};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getContext(), aList, R.layout.listview_activity, from, to);
+        listView.setAdapter(simpleAdapter);
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // deleteItem
+                SwipeMenuItem deleteItem = new SwipeMenuItem(getContext());
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xcc, 0x00, 0x00)));
+                deleteItem.setWidth(120);
+                deleteItem.setTitle("Delete");
+                deleteItem.setTitleSize(18);
+                deleteItem.setTitleColor(Color.WHITE);
+                menu.addMenuItem(deleteItem);
             }
+        };
+        listView.setMenuCreator(creator);
 
-            String[] from = { "listview_title", "listview_discription", "listview_item"};
-            int[] to = {R.id.lstTime, R.id.lstTitle, R.id.lstItems};
+        listView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
 
-            SimpleAdapter simpleAdapter = new SimpleAdapter(getContext(), aList, R.layout.listview_activity, from, to);
-            ListView androidListView = (ListView) view.findViewById(R.id.lst);
-            androidListView.setAdapter(simpleAdapter);
+                        Log.d(TAG, "onMenuItemClick: clicked item " + index);
+                        break;
+                  /*  case 1:
+                        Log.d(TAG, "onMenuItemClick: clicked item " + index);
+                        break; */
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
 
         return view;
     }
