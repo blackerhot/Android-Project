235a2faf84d47bc91da7ef7fd4671963378ca769
wanchana anurakmontri <wanchana@kkumail.com>
2019-05-11 14:57:30 +0700

    add button

--- app/src/main/java/th/ac/kku/asayaporn/project/ItemActivity.java
+++ app/src/main/java/th/ac/kku/asayaporn/project/ItemActivity.java
@@ -10,7 +10,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.CalendarContract;
 import android.support.annotation.Nullable;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -18,9 +17,8 @@ import android.util.Log;
 import android.view.Display;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -41,8 +39,6 @@ import com.google.api.services.calendar.model.EventAttendee;
 import com.google.api.services.calendar.model.EventDateTime;
 import com.google.api.services.calendar.model.EventReminder;
 import com.google.api.services.calendar.model.Events;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
@@ -63,8 +59,8 @@ public class ItemActivity extends AppCompatActivity {
     TextView detail;
     ImageView pic;
     TextView title;
-    TextView phone;
-    TextView website;
+    ImageButton phone;
+    ImageButton website;
     TextView sponser;
     com.google.api.services.calendar.Calendar mService;
     final JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
@@ -74,7 +70,7 @@ public class ItemActivity extends AppCompatActivity {
     GoogleSignInAccount googleSignInAccount;
     Bundle para;
     final HttpTransport transport = AndroidHttp.newCompatibleTransport();
-
+    Intent intentother = null;
     private static final String[] SCOPES = {CalendarScopes.CALENDAR};
 
     @Override
@@ -89,15 +85,13 @@ public class ItemActivity extends AppCompatActivity {
         title = (TextView) findViewById(R.id.post_title);
         detail = (TextView) findViewById(R.id.post_detail);
         datest = (TextView) findViewById(R.id.post_date);
-        phone = (TextView) findViewById(R.id.post_phone);
-        website = (TextView) findViewById(R.id.post_website);
+        phone = (ImageButton) findViewById(R.id.post_phone);
+        website = (ImageButton) findViewById(R.id.post_website);
        sponser = (TextView) findViewById(R.id.post_sponser);
         address = (TextView) findViewById(R.id.post_address);
         butAddEvent = (Button) findViewById(R.id.butAddEvent);
         googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this);
         para = getIntent().getExtras();
-        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collap);
-        getSupportActionBar().setTitle(para.getString("title"));
 
 
         Display display = getWindowManager().getDefaultDisplay();
@@ -106,40 +100,57 @@ public class ItemActivity extends AppCompatActivity {
         int width = size.x;
         Picasso.get().load(para.getString("img")).resize(width, 0).into(pic);
 
-
         title.setText(para.getString("title"));
-        website.setText(para.getString("website"));
+        getSupportActionBar().setTitle("");
+
+        //website.setText(para.getString("website"));
         sponser.setText(para.getString("sponsor"));
         detail.setText(para.getString("detail"));
         datest.setText(para.getString("datest") +" ,  "+ para.getString("dateend"));
-        phone.setText(para.getString("phone"));
+        //phone.setText(para.getString("phone"));
         address.setText(para.getString("address"));
 
-        final FirebaseUser currentFirebaseUser =  FirebaseAuth.getInstance().getCurrentUser();
-        if(currentFirebaseUser!=null) {
-            credentialCaledndar = GoogleAccountCredential.usingOAuth2(
-                    getApplicationContext(), Arrays.asList(SCOPES))
-                    .setBackOff(new ExponentialBackOff())
-                    .setSelectedAccount(googleSignInAccount.getAccount());
-            mService = new com.google.api.services.calendar.Calendar.Builder(
-                    transport, jsonFactory, credentialCaledndar)
-                    .setApplicationName("Google Calendar API Android Quickstart")
-                    .build();
-            create = false;
-            new ApiAsyncTask(ItemActivity.this).execute();
+        credentialCaledndar = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff())
+                .setSelectedAccount(googleSignInAccount.getAccount());
 
-        }
+
+        mService = new com.google.api.services.calendar.Calendar.Builder(
+                transport, jsonFactory, credentialCaledndar)
+                .setApplicationName("Google Calendar API Android Quickstart")
+                .build();
+
+        create = false;
+        new ApiAsyncTask(ItemActivity.this).execute();
+
         butAddEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(currentFirebaseUser!=null) {
-                    create = true;
-                    new ApiAsyncTask(ItemActivity.this).execute();
-                }else{
-                    Toast.makeText(ItemActivity.this,"Please using google account only",Toast.LENGTH_SHORT).show();
-                }
+                create = true;
+
+                new ApiAsyncTask(ItemActivity.this).execute();
+
             }
         });
+        website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentother = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(para.getString("website")));
+                startActivity(intentother);
+            }
+        });
+        phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentother = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse("tel:" + para.getString("phone")));
+                startActivity(intentother);
+            }
+        });
+
+
 
     }
     void showGooglePlayServicesAvailabilityErrorDialog(
